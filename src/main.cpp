7ade7a51d8dc495e05dd3a57e72ad7b17e7aaac2
Daniel Velasco <divelasco7@gmail.com>
2024-12-14 21:00:10 -0600

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+//Sidh Is cracked at coding
+//(foward 200 __FLT_EVAL_METHOD__)
+
+#include "vex.h"
+using namespace vex;
+
+
+void auton()
+{
+
+}
+
+void driver()
+{
+
+}
+
+int main()
+{
+
+  
+  while (true)
+  {
+    wait(20, msec);
+  }
+  return 0;
+}
